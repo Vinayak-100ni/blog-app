f343a8e56c64ecbd14f4fabafa33817f5473e486
Vinayak soni <112276881+Vinayak-100ni@users.noreply.github.com>
2024-01-11 23:36:26 +0530

    Update Post.jsx

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -34,34 +34,36 @@ export default function Post() {
 
  return post ? (
   <div className="py-8">
-   <Container>
-    <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
-     <img
-      src={appwriteService.getFilePreview(post.contentImg)}
-      alt={post.title}
-      className="rounded-xl"
-     />
+      <Container>
+        <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2 overflow-hidden hover:shadow-lg transition-all duration-300">
+          <img
+            src={appwriteService.getFilePreview(post.contentImg)}
+            alt={post.title}
+            className="rounded-xl"
+          />
 
-     {isAuthor && (
-      <div className="absolute right-6 top-6">
-       <Link to={`/edit-post/${post.$id}`}>
-        <Button bgColor="bg-green-500" className="mr-3">
-         Edit
-        </Button>
-       </Link>
-       <Button bgColor="bg-red-500" onClick={deletePost}>
-        Delete
-       </Button>
-      </div>
-     )}
+          {isAuthor && (
+            <div className="absolute right-6 top-6 flex space-x-2 items-center">
+              <Link to={`/edit-post/${post.$id}`}>
+                <Button bgColor="bg-green-500">Edit</Button>
+              </Link>
+              <Button
+                bgColor="bg-red-500"
+                onClick={deletePost}
+                className="transform hover:scale-110 transition-all duration-300"
+              >
+                Delete
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className="w-full mb-6">
+          <h1 className="text-3xl font-bold">{post.title}</h1>
+        </div>
+        <div className="browser-css">
+          {parse(post.content)}
+        </div>
+      </Container>
     </div>
-    <div className="w-full mb-6">
-     <h1 className="text-2xl font-bold">{post.title}</h1>
-    </div>
-    <div className="browser-css">
-     {parse(post.content)}
-    </div>
-   </Container>
-  </div>
  ) : null;
 }
